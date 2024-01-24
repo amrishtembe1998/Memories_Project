c79aa1119b3abf4903587256c03dcefc3442528c
amrishtembe1998 <amrishtembe1998@gmail.com>
2024-01-24 12:33:52 +0530

    Added redux & created form

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,7 +4,7 @@ export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
 
-    console.log(postMessages);
+    //console.log(postMessages);
 
     res.status(200).json(postMessages);
   } catch (error) {
@@ -14,12 +14,14 @@ export const getPosts = async (req, res) => {
 
 export const createPost = async (req, res) => {
   const post = req.body;
+  console.log("Post is: ", post);
   const newPost = new PostMessage(post);
+  console.log("newPost is: ", newPost);
   try {
     await newPost.save();
-
     res.status(200).json(newPost);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    //console.log(error);
+    res.status(409).json({ message: error });
   }
 };
