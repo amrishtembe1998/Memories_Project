c90e95d504a486c8fa47d022d55ccfcd51fb4db3
amrishtembe1998 <amrishtembe1998@gmail.com>
2024-01-26 10:08:42 +0530

    Deployment changes

--- server/index.js
+++ server/index.js
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+
+app.get("/", (req, res) => res.send("Hi this is Memories App"));
 const PORT = process.env.PORT || 5000;
 
 mongoose
