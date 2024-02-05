7ffecffed2520339a51af06740486e4cbf58a49e
amrishtembe1998 <amrishtembe1998@gmail.com>
2024-02-05 10:26:00 +0530

    Test change

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-app.get("/", (req, res) => res.send("Hi this is Memories App"));
+app.get("/", (req, res) => res.send("Hi this is Memories App!"));
 const PORT = process.env.PORT || 5000;
 
 mongoose
